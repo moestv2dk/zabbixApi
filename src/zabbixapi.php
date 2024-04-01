c7eabd33a79e7bb3d220121719f354bc98566984
Morten Moesgaard <moes@tv2.dk>
2024-04-01 16:29:21 +0200

    preparing for module to be part of composer

--- src/zabbixapi.php
+++ src/zabbixapi.php
@@ -1,72 +1,99 @@
 <?php
-class ZabbixApi {
-    private $username = ""; 
-    private $password = "";  
+
+namespace ZabbixApi\ZabbixApi;
+
+class ZabbixApi
+{
+    /**
+     *  username
+     */
+    private $username = "";
+    private $password = "";
     public $apitoken = "notset"; //default value
     public $zbxhost = '';  //we add  this  for  the  description  of the trigger
-    public $zabbixhost = ''; // out  call for api
+    public $zabbixhost = ''; // call for api
     public $triggers = 'notset';
     public $hosts = 'notset';
 
-    function __construct($info = "hosts", $keys = '') {
-	// set globals
-      global $ZabbixApi,$zabbixUser,$zabbixPass,$zabbixHost,$zabbixUrl,$Convert,$Groupids;
+    public function __construct($info = "hosts", $keys = '')
+    {
         //Log in to zabbix
-        $this->username = $zabbixUser;
-        $this->password = $zabbixPass;
-        $this->zbxhost = $zabbixHost;
-        $this->zabbixhost = $zabbixUrl;
+        $this->username = ZABBIXUSER;
+        $this->password = ZABBIXPASS;
+        // if API token is set via constant we wont use User and password
+        $this->apitoken = ZABBIXTOKEN;
+
+        $this->zbxhost = ZABBIXHOST;
+        $this->zabbixhost = ZABBIXURL;
+
         $this->login();
+
     }
-    public function curlie($data_string) {
-        // we  build the  curl options for contacting the  zabbix host 
+    public function curl_request($data_string): object
+    {
+        // we  build the  curl options for contacting the  zabbix host
+
         $ch = curl_init($this->zabbixhost);
+
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-            'Accept: application/json', 'Content-Type: application/json'
+          'Accept: application/json', 'Content-Type: application/json'
         ));
+
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_VERBOSE, true);
+
         $result = curl_exec($ch);
         $info = curl_getinfo($ch);
-        //execute the curl and close the connection  
+
+        //execute the curl and close the connection
         curl_close($ch);
         return $result;
     }
-    function login() {
+    public function login()
+    {
         //we  detect if  the API token is default value, if it is not, we dont need to log in again.
         if ($this->apitoken == 'notset') {
+
             $jsoninfo = '{ "jsonrpc":"2.0","method": "user.login", "params" :{ "user": "' . $this->username . '", "password": "' . $this->password . '"},"id":1}';
+
             $test = $this->curlie($jsoninfo);
+
             $files = json_decode($test);
+
             //set api token so we can log in
-		$this->apitoken = $files->result;
+
+            $this->apitoken = $files->result;
         }
     }
-    function apiBuilder($method,$params,$addons){
-     //we build  the array  that is need for  the contact with Zabbix    
+    public function apiBuilder($method, $params, $addons): string
+    {
+        //we build  the array  that is need for  the contact with Zabbix
         $array = array();
         $array['jsonrpc'] = '2.0';
         $array['method'] = $method;
         $array['params'] = $params;
-        if(is_array($addons)){
-            array_merge($array,$addons);
-        }   
+        if (is_array($addons)) {
+            array_merge($array, $addons);
+        }
         $array['auth'] = $this->apitoken;
         $array['id'] = 1;
         //we return  the converted array to json
-        return json_encode($array);  
+        return json_encode($array);
     }
-    public function get_zabbix_req($method,$params,$addons) {
+    public function get_zabbix_req($method, $params, $addons): object
+    {
         // we ask to get our arrays converted to json
-        $jsoninfo = $this->apiBuilder($method,$params,$addons);
-        // we fire off our json request and then return the  Json decoded content  
-        $files = json_decode($this->curlie($jsoninfo));
+        $jsoninfo = $this->apiBuilder($method, $params, $addons);
+
+        // we fire off our json request and then return the  Json decoded content
+        $files = json_decode($this->curl_request($jsoninfo));
+
         return $files;
     }
-    function __destruct() {
+    public function __destruct()
+    {
     }
 }
-$ZabbixApi = new ZabbixApi();
